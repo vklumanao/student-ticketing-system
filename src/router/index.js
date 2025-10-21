e816226ef90f2f4f30b5bc544f2e038f5a996830
vklumanao <wariyuri3@gmail.com>
2025-10-21 23:24:26 +0800

    fix: restructuring the folders

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import CreateTicketView from '@/views/CreateTicketView.vue'
-import TrackTicketView from '@/views/TrackTicketView.vue'
-import AdminView from '@/views/AdminView.vue'
+import HomeView from '../views/system/HomeView.vue'
+import CreateTicketView from '@/views/system/CreateTicketView.vue'
+import TrackTicketView from '@/views/system/TrackTicketView.vue'
+import AdminView from '@/views/system/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
